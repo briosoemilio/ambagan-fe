80027f16ddefee28307ddf3d6cd7ead46d2dfab7
develmo <brioso.emilioluis@gmail.com>
2026-04-03 15:05:46 +0800

    Add delete button

--- app/components/projects/ProjectCard.vue
+++ app/components/projects/ProjectCard.vue
@@ -6,9 +6,12 @@ const props = defineProps<{ project: Project }>()
 <template>
   <UCard class="cursor-pointer hover:bg-slate-200 dark:hover:bg-slate-700" variant="subtle">
     <div class="flex flex-col h-full">
-      <h2 class="text-xl font-semibold truncate mb-2">
-        {{ project.name }}
-      </h2>
+      <div class="flex justify-between items-start">
+        <h2 class="text-xl font-semibold truncate mb-2">
+          {{ project.name }}
+        </h2>
+        <UButton icon="i-heroicons-trash" color="error" variant="ghost" />
+      </div>
       <div class="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400 mb-4">
         <UAvatar :src="project.owner.avatar?.src" :alt="project.owner.name" size="xs" />
         <span>{{ project.owner.name }}</span>
